20a546e696ad0ed15f914d0a01fbda181d1b972d
Yaroslav <kznkv@gmail.com>
2024-06-30 19:56:46 +0300

    використували створення елементів через JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const h1 = <h1 id="title">Hello App</h1>
+const p = (
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, nemo a
+        nihil ea adipisci veniam debitis eligendi nostrum at, possimus aliquid
+        qui mollitia quibusdam illum blanditiis dolore cupiditate sequi! Fugiat!
+    </p>
+)
+
+const ul = (
+    <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+    </ul>
+)
+
+const app = (
+    <div>
+        {h1}
+        {p}
+        {ul}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
