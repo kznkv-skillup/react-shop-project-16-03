5a7b9701a1ceda680134741cbea429d9804d18ea
Yaroslav <kznkv@gmail.com>
2024-06-30 20:19:10 +0300

    +created React Component (Arrow func)

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = <h1 id="title">Hello App</h1>
-const p = (
+// Arrow Function
+const Title = () => <h1>Hello Title Component</h1>
+
+const Content = () => (
     <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, nemo a
-        nihil ea adipisci veniam debitis eligendi nostrum at, possimus aliquid
-        qui mollitia quibusdam illum blanditiis dolore cupiditate sequi! Fugiat!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime hic
+        facilis libero assumenda aperiam dolor animi nobis suscipit tempora vel,
+        explicabo iusto laborum? Similique ratione rem quia. Consequuntur, nobis
+        doloribus.
     </p>
 )
 
-const ul = (
-    <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
-        <li>List item 3</li>
-    </ul>
-)
-
-const app = (
-    <div>
-        {h1}
-        {p}
-        {ul}
-    </div>
-)
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <Title />
+        <Content />
+    </React.StrictMode>
+)
