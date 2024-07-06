a7871303969e13b2972988efee08ce025960bd60
Yaroslav <kznkv@gmail.com>
2024-07-06 18:25:47 +0300

    +added basic arrow func components

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,34 @@ import ReactDOM from 'react-dom/client'
 const Title = () => <h1>Hello Title Component</h1>
 
 const Content = () => (
-    <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime hic
-        facilis libero assumenda aperiam dolor animi nobis suscipit tempora vel,
-        explicabo iusto laborum? Similique ratione rem quia. Consequuntur, nobis
-        doloribus.
-    </p>
+    <div>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime hic
+            facilis libero assumenda aperiam dolor animi nobis suscipit tempora
+            vel, explicabo iusto laborum? Similique ratione rem quia.
+            Consequuntur, nobis doloribus.
+        </p>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime hic
+            facilis libero assumenda aperiam dolor animi nobis suscipit tempora
+            vel, explicabo iusto laborum? Similique ratione rem quia.
+            Consequuntur, nobis doloribus.
+        </p>
+    </div>
 )
+
+const App = () => {
+    return (
+        <div>
+            <Title />
+            <Content />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <Title />
-        <Content />
+        <App />
     </React.StrictMode>
 )
