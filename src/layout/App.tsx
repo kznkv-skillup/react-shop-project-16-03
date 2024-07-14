0aee7ef5b3ee3d916feb8ed9d9a7436fd5b26cb8
Yaroslav <kznkv@gmail.com>
2024-07-14 20:01:50 +0300

    +added absolute imports and Menu,Logo components

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,11 +1,15 @@
 import Header from './Header'
 import CssBaseline from '@mui/material/CssBaseline'
+import Main from './Main'
+import Footer from './Footer'
 
 const App = () => {
     return (
         <>
             <CssBaseline />
             <Header />
+            <Main />
+            <Footer />
         </>
     )
 }
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,8 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
-import logo from '../assets/logo.svg'
+import Logo from '../components/Logo/Logo'
+import Menu from '../components/Menu/Menu'
 
 type Props = {}
 
@@ -19,26 +16,8 @@ const Header = (props: Props) => {
         >
             <Container>
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        <img src={logo} alt="" />
-                    </Typography>
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">Products</Button>
-                    <Button color="inherit">Payment</Button>
-                    <Button color="inherit">Cart</Button>
+                    <Logo />
+                    <Menu />
                 </Toolbar>
             </Container>
         </AppBar>
