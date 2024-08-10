349fe3e77faf7586eebed2884a8846ffbc3e147f
Yaroslav <kznkv@gmail.com>
2024-08-10 20:23:35 +0300

    +added total count

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -15,12 +15,25 @@ const CartHeader = ({
 }: Props) => {
     return (
         <div>
-            {Object.keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productsObject[+productId].title}:{' '}
-                    {productsInCart[+productId]}
-                </div>
-            ))}
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[+productId].title}:{' '}
+                        {productsInCart[+productId]}
+                    </div>
+                ))}
+            </div>
+            <div>
+                Total:{' '}
+                {Object.keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsInCart[+productId] *
+                            productsObject[+productId].price,
+                    0
+                )}
+                $
+            </div>
         </div>
     )
 }
