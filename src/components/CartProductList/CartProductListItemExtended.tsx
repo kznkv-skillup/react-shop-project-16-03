7660e612ed9fc116a2fc8e2d51842e63275f453b
Yaroslav <kznkv@gmail.com>
2024-08-31 18:47:35 +0300

    added like logic to cart

--- src/components/CartProductList/CartProductListItemExtended.tsx
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -2,6 +2,10 @@ import { Button, Card, CardContent, Grid } from '@mui/material'
 import { Product } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Quantity from 'components/Quantity/Quantity'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import { useAppDispatch, useAppSelector } from 'store/hooks'
+import { addLike, removeLike } from 'store/likeSlice'
 
 type Props = {
     product: Product
@@ -16,6 +20,11 @@ const CartProductListItemExtended = ({
     removeProductFromCart,
     changeProductQuantity,
 }: Props) => {
+    const isLiked = useAppSelector(
+        (state) => state.productsLikeState[product.id]
+    )
+
+    const dispatch = useAppDispatch()
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Card variant="outlined">
@@ -23,7 +32,23 @@ const CartProductListItemExtended = ({
                     <div className="product-image">
                         <img src={product.image} alt="" />
                     </div>
-                    <div>{product.title}</div>
+                    <Button
+                        variant="outlined"
+                        onClick={() => {
+                            isLiked
+                                ? dispatch(removeLike(product.id))
+                                : dispatch(addLike(product.id))
+                        }}
+                    >
+                        {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                    </Button>
+                    <div
+                        style={{
+                            margin: '20px 0',
+                        }}
+                    >
+                        {product.title}
+                    </div>
                     <p>Price for one item: {product.price}</p>
                     <div>Count: {productsCount}</div>
                     <Quantity
